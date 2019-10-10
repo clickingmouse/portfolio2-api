41bd1a303c7feaae50e03bf6805b63df7cbe3cf4
clickingmouse <clickingmouse@users.noreply.github.com>
2019-10-10 13:44:36 +0800

    fixed models/Item typo

--- routes/api/itemRoutes.js
+++ routes/api/itemRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 //Item model
-const Item = require("../../models/Item");
+const Item = require("../../models/item");
 
 // @ route GET api/items
 // @ desc get all items
